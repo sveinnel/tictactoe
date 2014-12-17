534479a555dcbeece0cbcae7f2c83d0c99628bd4
Sveinnel <sveinnel@gmail.com>
2014-12-17 04:14:53 +0000

    acceptancetest script more ready and now all stages are automatic

--- dockeracceptancepush.sh
+++ dockeracceptancepush.sh
@@ -38,21 +38,21 @@ echo ------------------------------------------------
 echo  Done pulling new dockerimage
 echo
 
-# echo Killing old dockerimage...
-# ssh root@$ACCEPTANCE_URI 'docker kill tictactoe' 
-# EXITCODE=$?
-# if [ $EXITCODE -ne 0 ] ; then exit $EXITCODE ; fi
-# echo ------------------------------------------------
-# echo  Done killing old dockerimage
-# echo
+echo Killing old dockerimage...
+ssh root@$ACCEPTANCE_URI 'docker kill tictactoe' 
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ] ; then exit $EXITCODE ; fi
+echo ------------------------------------------------
+echo  Done killing old dockerimage
+echo
 
-# echo Removing old dockerimage...
-# ssh root@$ACCEPTANCE_URI 'docker rm tictactoe' 
-# EXITCODE=$?
-# if [ $EXITCODE -ne 0 ] ; then exit $EXITCODE ; fi
-# echo ------------------------------------------------
-# echo  Done removing old dockerimage
-# echo
+echo Removing old dockerimage...
+ssh root@$ACCEPTANCE_URI 'docker rm tictactoe' 
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ] ; then exit $EXITCODE ; fi
+echo ------------------------------------------------
+echo  Done removing old dockerimage
+echo
 
 echo Starting new dockerimage...
 ssh root@$ACCEPTANCE_URI 'docker run -p 80:8080 -d -e NODE_ENV=production -e MONGOLAB_URI='$MONGOLAB_URI' --name="tictactoe" sveinnel/tictactoe'
